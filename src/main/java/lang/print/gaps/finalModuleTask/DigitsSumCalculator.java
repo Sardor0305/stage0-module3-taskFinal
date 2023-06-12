8d8a9624776dc3eefc46e85d8d270d4d2568b2fd
Sardor0305 <87496899+Sardor0305@users.noreply.github.com>
2023-06-12 14:32:15 +0500

    Update DigitsSumCalculator.java

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,17 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int s = 0;
+        while (number>0){
+            s = s + number%10;
+
+            number = number/10;
+
+        }
+        System.out.println(s);
+
+
+
+    
     }
 }
